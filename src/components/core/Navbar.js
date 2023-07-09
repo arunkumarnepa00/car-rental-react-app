ce0b60ecf662a8e38fafe8101d8adb8280939889
arunkumarnepa00 <63899190+arunkumarnepa00@users.noreply.github.com>
2023-07-10 00:43:41 +0530

    navbar svg warnings removed

--- src/components/core/Navbar.js
+++ src/components/core/Navbar.js
@@ -33,8 +33,8 @@ export const Navbar=()=>{
               handleClose();
             }}>
               <svg width="20px" height="20px" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 21.32L21 3.32001" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                 <path d="M3 3.32001L21 21.32" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M3 21.32L21 3.32001" stroke="#FFFFFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                 <path d="M3 3.32001L21 21.32" stroke="#FFFFFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
             </div>
             {/* normal menu */}
